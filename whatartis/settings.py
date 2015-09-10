c5ab56b0cfd3321e0774a3a80e81840bce18b33d
Filipe Ximenes <filipeximenes@gmail.com>
2015-09-10 18:07:01 -0300

    The expression or application of creative skill and imagination, typically in a visual form such as painting, drawing, or sculpture, producing works to be appreciated primarily for their beauty or emotional power.

--- whatartis/settings.py
+++ whatartis/settings.py
@@ -13,8 +13,14 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+from decouple import config
+from dj_database_url import parse as db_url
+from unipath import Path
 
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+def base_dir_join(*args):
+    return os.path.join(BASE_DIR, *args)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
@@ -77,10 +83,11 @@ WSGI_APPLICATION = 'whatartis.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': config(
+    'DATABASE_URL',
+    default='sqlite:///' + base_dir_join('db.sqlite3'),
+    cast=db_url
+    )
 }
 
 
